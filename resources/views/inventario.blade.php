0cd367a41127b534999436931c808a9bc4b6c0e2
JohnArbaiza1 <johnarbaiza07.03@gmail.com>
2024-09-07 21:13:20 -0600

    Se agrega estilos a las views del dashboard

--- resources/views/inventario.blade.php
+++ resources/views/inventario.blade.php
@@ -3,106 +3,69 @@
 {{--Section para poder trabajar con layout --}}
 
 @section('content')
-    @vite('resources/js/app.js')
-    <div class="container-fluid" style="margin-top: 70px; margin-bottom: 70px; max-width: 950px;">
-        <div class="row bg-white">
-            <div class="col-12">
-                <div class="row text-center">
-                    <h2>Informe de inventario</h2>
-                </div>
-            </div>
-            <div class="col-2"></div>
-            <div class="col8">
-                <div class="">
-                    <form action="{{ route("registro-inventario") }}" method="get" id="form_inventario">
-                        @csrf
-                        <div class="row mb-5 d-flex justify-content-center align-items-center">
-                            <div class="col-md-5 col-12">
-                                <p>Número de sala</p>
-                                <select name="room" class="form-select" id="room">
-                                    <option value="0">Seleccionar sala</option>
-
-                                </select>
-                            </div>
-                            <div class="col-md-1 col-12"></div>
-                            <div class="col-md-5 col-12">
-                            </div>
-                        </div>
-                        <div class="row mb-5 d-flex justify-content-center align-items-center">
-                            <div class="col-md-5 col-12">
-                                <p>Tipo de recurso</p>
-                                <select name="resource_type" class="form-select" id="resource_type">
-                                    <option value="0">Seleccionar tipo de recurso</option>
-                                </select>
-                            </div>
-                            <div class="col-md-1 col-12"></div>
-                            <div class="col-md-5 col-12">
-                                <p>Tipo de estado</p>
-                                <select name="status" class="form-select status" id="status">
-                                    <option value="0">Seleccionar estado</option>
-                                    {{--<option value="1">Buen estado</option>
-                                    <option value="1">Mal estado</option>
-                                    <option value="1">Reparación</option>--}}
-                                </select>
-                            </div>
-                        </div>
-                        <div class="row mb-5 d-flex justify-content-center align-items-center">
-                            <div class="col-md-5 col-12">
-                                {{--<p>Numero de recurso</p>
-                                <select name="pc" class="form-select">
-                                    <option value="0">Seleccionar recurso</option>
-                                    <option value="1">Computadora 1</option>
-                                    <option value="1">Computadora 1</option>
-                                    <option value="1">Computadora 1</option>
-                                    <option value="2">Seleccion desde db</option>
-                                    <option value="2">Seleccion desde db</option>
-                                    <option value="2">Seleccion desde db</option>
-                                </select>--}}
-                            </div>
-                            <div class="col-md-1 col-12"></div>
-                            <div class="col-md-5 col-12">
-                                {{--<p>Tipo de estado</p>
-                                <select name="status_nam" class="form-select status">
-                                    <option value="0">Seleccionar estado</option>
-                                    --}}{{--<option value="1">Buen estado</option>
-                                    <option value="1">Mal estado</option>
-                                    <option value="1">Reparación</option>--}}{{--
-                                </select>--}}
-                            </div>
-                        </div>
-                        <div class="row d-flex justify-content-center align-items-center mb-3">
-                            <div class="col-md-5 col-12">
-                                {{--<div>
-                                    <p>Número de activo fijo</p>
-                                </div>
-                                <div class="form-floating">
-                                    <input type="number" name="numero_activo" class="form-control"
-                                           placeholder="Número de activo fijo" id="floatingInput" min="0">
-                                    <label for="floatingInput">Número de activo fijo</label>
-                                </div>--}}
-                            </div>
-                            <div class="col-md-1 col-12"></div>
-                            <div class="col-md-5 col-12">
-                                {{--<p>Computadora</p>
-                                <select name="pc_activo_fijo" class="form-select">
-                                    <option value="0">Seleccionar computadora</option>
-                                    <option value="1">Computadora 1</option>
-                                    <option value="2">Seleccion desde db</option>
-                                </select>--}}
-                            </div>
-                        </div>
-                        <div class="row d-flex justify-content-center align-items-center">
-                            <div class="col-md-4 col-12"></div>
-                            <div class="col-md-4 col-12">
-                                <input type="submit" value="Registrar inventario" class="btn text-white" style="width: 100%; background-color: #660D04;" id="submit">
-                            </div>
-                            <div class="col-md-4 col-12"></div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+<link rel="stylesheet" href="{{ asset('CSS/administracion/inventario.css')}}">
+<!--Titulo de la vista -->
+    <div class="container-fluid">
+        <div class="title">
+            <h2>Registro de Inventario de Equipos de VR</h2>
         </div>
     </div>
+<!-- Contenido de la vista -->
+    <div class="contentInventario">
+        <form action="{{ route("registro-inventario") }}" method="get" id="form_inventario">
+        @csrf
+            <div class="opcionesInventario">
+
+                <div class="col">
+                    <p>Número de sala</p>
+                        <select name="room" class="form-select" id="room">
+                            <option value="0">Seleccionar sala</option>
+                        </select>
+                </div>
+
+                <div class="col">
+                <p>Número de Activo Fijo</p>
+                   <input type="text" class="ActivoFijo" placeholder="112333">
+                </div>
+
+                <div class="col">
+                    <p>Tipo de recurso</p>
+                        <select name="resource_type" class="form-select" id="resource_type">
+                            <option value="0">Seleccionar tipo de recurso</option>
+                        </select>
+                </div>
+
+                <div class="col">
+                    <p>Tipo de estado</p>
+                        <select name="status" class="form-select status" id="status">
+                            <option value="0">Seleccionar estado</option>
+                            {{--<option value="1">Buen estado</option>
+                            <option value="1">Mal estado</option>
+                            <option value="1">Reparación</option>--}}
+                        </select>
+                </div>
+
+                <div class="col">
+                    {{--<p>Numero de recurso</p>
+                            <select name="pc" class="form-select">
+                                <option value="0">Seleccionar recurso</option>
+                                <option value="1">Computadora 1</option>
+                                <option value="1">Computadora 1</option>
+                                <option value="1">Computadora 1</option>
+                                <option value="2">Seleccion desde db</option>
+                                <option value="2">Seleccion desde db</option>
+                                <option value="2">Seleccion desde db</option>
+                            </select>--}}
+                </div>
+
+                <div class="buttonContainer">
+                    <input type="submit" value="Registrar inventario" class="btn-success"  id="submit">
+                </div>
+
+            </div>
+        </form>
+    </div>
+
     <script type="module" src="{{asset("js/getResponsePromise.js")}}"></script>
     <script type="module" src="{{ asset("js/makeOptions.js") }}"></script>
 @endsection
--- resources/views/registro-inventario.blade.php
+++ resources/views/registro-inventario.blade.php
@@ -11,20 +11,20 @@
           integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
           crossorigin="anonymous" referrerpolicy="no-referrer"/>
     <div class="container-xl" style="margin-top: 70px; margin-bottom: 70px;">
-        <a href="{{route("inventario")}}" class="btn btn-success position-relative">
+        <a href="{{route("inventario")}}" class="btnInventario">
             <i class="fa-solid fa-vr-cardboard"></i>
-            <span class="tooltip-text">Registrar Inventario</span>
+            <span class="tooltip-text"> Registrar Inventario</span>
         </a>
     </div>
 
-    <div class="container-xl" style="margin-top: 70px; margin-bottom: 70px;">
+    <div class="container-xl">
         <div class="row justify-content-center">
             <div class="col-12">
                 <div class="card">
                     <div class="card-body">
                         <div class="table-responsive" style="height: 100%; width: 100%;">
                             <table class="table table-bordered text-center" style="width: 100%; margin-bottom: 0;" id="inventoryTable">
-                                <thead class="table-dark">
+                                <thead class="table-avatar">
                                     <tr>
                                         <th scope="col">#</th>
                                         <th scope="col">Número de sala</th>
