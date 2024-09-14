6988634abf57439c972b99da8696feb88895fc34
Alex Ulloa <US21003@ues.edu.sv>
2024-09-14 08:42:44 -0600

    Fixed appointments table size

--- resources/views/appointments/appointments.blade.php
+++ resources/views/appointments/appointments.blade.php
@@ -34,20 +34,66 @@ if (!isset($dashboard)) {
                     <span class="tooltip-text">Generar Reporte en PDF</span>
                 </a>
             </div>
+
+            <!-- Parte de la busqueda -->
+            <div class="busquedaCitas">
+                <form action="" id="searchForm">
+                    <div class="input-group">
+                        <input type="text" id="searchInput" class="form-control" placeholder=" Buscar...">
+                        <button type="submit" class="btn-primary">Buscar</button>
+                    </div>
+                </form>
+            </div>
         </div>
 
-        <!-- Parte de la busqueda -->
-        <div class="busquedaCitas">
-            <form action="" id="searchForm">
-                <div class="input-group">
-                    <input type="text" id="searchInput" class="form-control" placeholder=" Buscar...">
-                    <button type="submit" class="btn-primary">Buscar</button>
+        {{--<div class="card-header bg-white">
+            <div class="row align-items-center"> <!-- Ensure vertical alignment -->
+                <div class="col-6 col-md-8 text-center"> <!-- Adjust the size as needed -->
+                    <br>
+                    <h2>Todas las citas</h2>
+                    <br>
                 </div>
-            </form>
+                <div class="col-6 col-md-4">
+                    <form action="" id="searchForm">
+                        <div class="input-group">
+                            <input type="text" id="searchInput" class="form-control"
+                                   placeholder="Buscar...">
+                            <button type="submit" class="btn btn-success">Buscar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>--}}
+
+        <div class="card-header bg-white">
+            <h2 class="titleCitas">Todas tus Citas</h2>
+        </div>
+
+
+
+        <div class="card-body">
+            <div class="table-responsive" style="height: 100%; width: 100%;">
+                <table id="appointmentsTable" class="table table-bordered text-center"
+                       style="width: 100%; margin-bottom: 0;">
+                    <thead class="table-avatar">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Encargado</th>
+                        <th scope="col">Departamento</th>
+                        <th scope="col">Carrera</th>
+                        <th scope="col">Fecha</th>
+                        <th scope="col">Hora</th>
+                        <th scope="col">Participantes</th>
+                        <th scope="col">Acciones</th>
+                    </tr>
+                    </thead>
+                    <tbody></tbody>
+                </table>
+            </div>
         </div>
 
         <!-- Parte de la tabla -->
-        <div class="contentCita">
+        {{--<div class="contentCita">
             <div class="card">
                 <div class="card-header bg-white">
                     <h2 class="titleCitas">Todas tus Citas</h2>
@@ -74,7 +120,7 @@ if (!isset($dashboard)) {
                     </div>
                 </div>
             </div>
-        </div>
+        </div>--}}
     </div>
 
     <script>
