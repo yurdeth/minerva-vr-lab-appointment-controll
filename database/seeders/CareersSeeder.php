f764a1af882b83347ffbbcb2f410a98a0fe90035
Alex Ulloa <US21003@ues.edu.sv>
2024-10-05 08:23:37 -0600

    Archivos json para cargar departamentos y carreras

--- database/seeders/CareersSeeder.php
+++ database/seeders/CareersSeeder.php
@@ -2,16 +2,43 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Careers;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Log;
 
-class CareersSeeder extends Seeder
-{
+class CareersSeeder extends Seeder {
     /**
      * Run the database seeds.
+     * @throws \Exception
      */
-    public function run(): void
-    {
-        //
+    public function run(): void {
+        try {
+            $careers = $this->readJsonCareers();
+            foreach ($careers as $career) {
+                Careers::create($career);
+            }
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            // O puedes lanzar la excepción si prefieres que falle el seeder
+            throw $e;
+        }
+    }
+
+    private function readJsonCareers(): array {
+        $jsonCareers = file_get_contents(base_path('/careers.json'));
+
+        // Verifica si el archivo fue leído correctamente
+        if ($jsonCareers === false) {
+            throw new \Exception('Archivo no encontrado o error al leerlo');
+        }
+
+        $departmentsArray = json_decode($jsonCareers, true);
+
+        // Verifica si hay errores al decodificar JSON
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new \Exception('Error al decodificar JSON: ' . json_last_error_msg());
+        }
+
+        return $departmentsArray['careers']; // Retorna solo el array de departamentos
     }
 }
--- database/seeders/DepartmentsSeeder.php
+++ database/seeders/DepartmentsSeeder.php
@@ -2,16 +2,43 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Departments;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Log;
 
-class DepartmentsSeeder extends Seeder
-{
+class DepartmentsSeeder extends Seeder {
     /**
      * Run the database seeds.
+     * @throws \Exception
      */
-    public function run(): void
-    {
-        //
+    public function run(): void {
+        try {
+            $departments = $this->readJsonDepartments();
+            foreach ($departments as $department) {
+                Departments::create($department);
+            }
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            // O puedes lanzar la excepción si prefieres que falle el seeder
+            throw $e;
+        }
+    }
+
+    private function readJsonDepartments(): array {
+        $jsonDepartments = file_get_contents(base_path('/departments.json'));
+
+        // Verifica si el archivo fue leído correctamente
+        if ($jsonDepartments === false) {
+            throw new \Exception('Archivo no encontrado o error al leerlo');
+        }
+
+        $departmentsArray = json_decode($jsonDepartments, true);
+
+        // Verifica si hay errores al decodificar JSON
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new \Exception('Error al decodificar JSON: ' . json_last_error_msg());
+        }
+
+        return $departmentsArray['departments']; // Retorna solo el array de departamentos
     }
 }
