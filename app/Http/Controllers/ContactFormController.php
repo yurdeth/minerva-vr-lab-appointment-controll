5c82d868a1968fae533ad1c053ea61c6b32ec726
Alex Ulloa <US21003@ues.edu.sv>
2024-10-21 15:38:06 -0600

    Implementing ContactFormController validations

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -11,6 +11,35 @@ use Illuminate\Support\Facades\Mail;
 
 class ContactFormController extends Controller {
     public function sendEmail(Request $request): JsonResponse{
+
+        if(!$request->subject || $request->subject == null){
+            return response()->json([
+                'message' => 'El asunto es requerido',
+                'success' => false
+            ]);
+        }
+
+        if(!$request->email || $request->email == null){
+            return response()->json([
+                'message' => 'El correo es requerido',
+                'success' => false
+            ]);
+        }
+
+        if(!$request->message || $request->message == null){
+            return response()->json([
+                'message' => 'El mensaje es requerido',
+                'success' => false
+            ]);
+        }
+
+        if(!preg_match("/^[a-zA-ZñÑáéíóúÁÉÍÓÚ ]*$/", $request->email)){
+            return response()->json([
+                'message' => 'El email no puede contener caracteres especiales',
+                'success' => false
+            ]);
+        }
+
         $details = [
             'subject' => $request->subject,
             'name' => 'Minerva RV Lab',
